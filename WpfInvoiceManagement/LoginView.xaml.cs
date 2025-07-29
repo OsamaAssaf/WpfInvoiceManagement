74fda07e2b33ce6bfdef3ddf97215ccc2cd97627
Osama Assaf <96818454+OsamaAssaf@users.noreply.github.com>
2025-07-29 14:24:18 +0300

    commit

---
+++ WpfInvoiceManagement/LoginView.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfInvoiceManagement
+{
+    public partial class LoginView : UserControl
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string passwordEnterned = PasswordBox.Password;
+
+            if (string.IsNullOrEmpty(passwordEnterned)) return;
+
+            string? envPassword = Environment.GetEnvironmentVariable("InvoiceManagementPassword");
+
+            if (envPassword == null)
+            {
+                MessageBox.Show("Environment Variable \"InvoiceManagementPassword\" Is Not Set");
+                return;
+            }
+
+            if (passwordEnterned == envPassword)
+            {
+                MessageBox.Show("Login successful!");
+            }
+            else
+            {
+                MessageBox.Show("Invalid password. Please try again.");
+            }
+        }
+
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            string passwordEnterned = PasswordBox.Password;
+            LoginButton.IsEnabled = !string.IsNullOrEmpty(passwordEnterned);
+        }
+    }
+}
